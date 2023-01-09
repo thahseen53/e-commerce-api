e2dc5d92c0ac14df52a96d7e54e6ea3e3677f911
thahseen53 <thahseen53@gmail.com>
2023-01-09 09:59:24 +0530

    ..

--- routes/cart.js
+++ routes/cart.js
@@ -5,66 +5,25 @@ const {
   verifyTokenAndAdmin,
 } = require("../middleware/verifyToken");
 
+
 const router = require("express").Router();
 
+const cartController = require('../controllers/cart')
 //CREATE
 
-router.post("/", verifyToken, async (req, res) => {
-  const newCart = new Cart(req.body);
-
-  try {
-    const savedCart = await newCart.save();
-    res.status(200).json(savedCart);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.post("/", verifyToken,cartController.createCart)
 
 //UPDATE
-router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
-  try {
-    const updatedCart = await Cart.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: req.body,
-      },
-      { new: true }
-    );
-    res.status(200).json(updatedCart);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.put("/:id", verifyTokenAndAuthorization, cartController.updateCart)
 
 //DELETE
-router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
-  try {
-    await Cart.findByIdAndDelete(req.params.id);
-    res.status(200).json("Cart has been deleted...");
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.delete("/:id", verifyTokenAndAuthorization,cartController.deleteCart)
 
 //GET USER CART
-router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
-  try {
-    const cart = await Cart.findOne({ userId: req.params.userId });
-    res.status(200).json(cart);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.get("/find/:userId", verifyTokenAndAuthorization, cartController.findUser);
 
 // //GET ALL
 
-router.get("/", verifyTokenAndAdmin, async (req, res) => {
-  try {
-    const carts = await Cart.find();
-    res.status(200).json(carts);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.get("/", verifyTokenAndAdmin, cartController.getAll)
 
 module.exports = router;
